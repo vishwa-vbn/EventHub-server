040e23caf7e5a35549d51d74f9a0b5e4592cf86d
Vishwa <77456055+vishwa-vbn@users.noreply.github.com>
2023-09-19 19:22:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://event-hub-server.vercel.app",
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
   })
